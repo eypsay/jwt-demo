22e855a8d32ea9972744e41d72fe7c7cd73ef9e7
eyp <eypsay@yahoo.com>
2022-10-02 01:03:07 +0300

    token part added

--- jwt-demo/src/main/java/sayilir/coder/jwtdemo/JwtDemoApplication.java
+++ jwt-demo/src/main/java/sayilir/coder/jwtdemo/JwtDemoApplication.java
@@ -2,7 +2,10 @@ package sayilir.coder.jwtdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import sayilir.coder.jwtdemo.config.RsaKeyProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class JwtDemoApplication {
 
